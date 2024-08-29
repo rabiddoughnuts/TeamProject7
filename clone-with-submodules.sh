e7b009f0f4931841701561a304c7dfe2c464ceea
ReeseHatfield <hatfield.69@wright.edu>
2024-08-29 08:56:43 -0400

    update-submodules script

--- clone-with-submodules.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-REPO="https://github.com/rabiddoughnuts/TeamProject7.git"
-
-git clone --recurse-submodules "$REPO"
-
-cd "$(basename "$REPO" .git)"
-
-git submodule update --init --recursive
---
+++ update-submodules.sh
@@ -0,0 +1,3 @@
+echo "Cloning sub modmodules ..."
+git submodule update --init --recursive
+echo "Done"
