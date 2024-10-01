99511ac233b1ed501b554998d5db59fc6630ab48
ReeseHatfield <hatfield.69@wright.edu>
2024-10-01 10:33:55 -0400

    format

--- image-detection/src/train-detection.py
+++ image-detection/src/train-detection.py
@@ -1,17 +1,17 @@
 from ultralytics import YOLO
 import sys
 
-model = YOLO('yolov8n.pt') # default pre trained  
+model = YOLO('yolov8n.pt')  # default pre trained
 epochs = int(sys.argv[1])
 
 model.train(
-    data='./roboflow/data.yaml',    
-    epochs=epochs,   
-    imgsz=640,     
-    batch=16,              
-    workers=4              
+    data='./roboflow/data.yaml',
+    epochs=epochs,
+    imgsz=640,
+    batch=16,
+    workers=4
 )
 
 model.val()
 
-model.export(format='onnx') 
+model.export(format='onnx')
