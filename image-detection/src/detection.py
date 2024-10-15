873283dbd0fd5adc177cd9a8685ba9d9b69803b7
ReeseHatfield <hatfield.69@wright.edu>
2024-10-15 08:50:07 -0400

    only take best imgs

--- image-detection/src/detection.py
+++ image-detection/src/detection.py
@@ -1,10 +1,12 @@
+import operator
 from ultralytics import YOLO
 import sys
 import os
-import shutil
 
 
 def main():
+    
+    images_with_death_star_detected = {}
 
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
@@ -20,7 +22,6 @@ def main():
 
     for i, file in enumerate(os.listdir(input_dir)):
         
-        
         filename = os.path.join(input_dir, file)
         # print(filename)
         result = model(filename)
@@ -29,25 +30,32 @@ def main():
 
         detected_death_star = len(boxes) != 0
         
-        
-        
         if detected_death_star:
             
             prefix = str(prefix_index).zfill(2)
             prefix_index += 1
             
             # this may need to like do the cropping n stuff here. but for now, this works
-            new_path = os.path.join(output_dir, f"{prefix}-{file}")
-            print(result[0].boxes.conf)
+            confidence_scores = result[0].boxes.conf
+            best_conf = max(confidence_scores)
             
-            result[0].save(new_path)
-            # new_path = os.path.join("./out", file)
-            # shutil.copyfile(filename, new_path)
+            print(f"Best confidence for {file}: {best_conf}")
+            
+            # store the name and the result type to use later
+            images_with_death_star_detected[(filename, result[0])] = best_conf
+
+    sorted_images = sorted(images_with_death_star_detected.items(), key=lambda x: x[1], reverse=True)
+
+    top_10_images = sorted_images[:10] # last 10, goes from lower conf -> high conf
+
+    # horrible iterator im sorry
+    for prefix_index, ((image_path, result), conf) in enumerate(top_10_images):
+        file = os.path.basename(image_path)
+        prefix = str(prefix_index).zfill(2)
+        new_path = os.path.join(output_dir, f"{prefix}-{file}")
         
-
-    # results = model('examples/testMe.jpeg')
-
-    # results[0].save("results.png")
+        result.save(new_path)
+        print(f"Saved {new_path} with confidence: {conf}")
 
 
 if __name__ == "__main__":
