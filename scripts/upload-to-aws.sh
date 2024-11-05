eb8260b22ace6e6f8ed95b3822b2a746daf143df
ReeseHatfield <hatfield.69@wright.edu>
2024-11-05 09:08:08 -0500

    fix thing

--- scripts/upload-to-aws.sh
+++ scripts/upload-to-aws.sh
@@ -15,3 +15,8 @@ remote_upload_path="/home/ubuntu/Classes/TeamProjects/SeniorDesignWebsite/backen
 
 scp -r "$local_upload_path"/* aws:"$remote_upload_path"
 echo "Done transferring files to aws"
+
+ssh aws
+
+cd /home/ubuntu/Classes/TeamProjects/SeniorDesignWebsite/backend
+npm run dev
